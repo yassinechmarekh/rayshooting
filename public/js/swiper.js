f44c8d22de642e7ba20a5be512c9d22a556fcc5e
yassine <yassinechmarekh88@gmail.com>
2024-09-18 00:31:59 +0100

    add tarif section to home page

--- public/js/swiper.js
+++ public/js/swiper.js
@@ -14,3 +14,25 @@ var swiper = new Swiper(".hero-swp", {
   },
   speed: 1000,
 });
+
+
+// tarif section on home page
+var swiper = new Swiper(".tarif-swp", {
+  loop: true,
+  autoplay: {
+    delay: 3500,
+  },
+  slidesPerView: 3,
+  spaceBetween: 20,
+  breakpoints: {
+    950: {
+      slidesPerView: 3,
+    },
+    550: {
+      slidesPerView: 2,
+    },
+    0: {
+      slidesPerView: 1,
+    },
+  },
+});
